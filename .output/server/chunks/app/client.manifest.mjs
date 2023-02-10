5560529817c3b01cde180197da604b02e4183f94
Adam Lee <adam@codelabx.ltd>
2023-02-10 16:12:28 +0000

    Fix build

---
+++ .output/server/chunks/app/client.manifest.mjs
@@ -0,0 +1,81 @@
+const client_manifest = {
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.css": {
+    "resourceType": "style",
+    "file": "error-404.dd29d79a.css",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.css"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-404.0e689212.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.0_@types+node@18.13.0/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue"
+  },
+  "error-404.dd29d79a.css": {
+    "file": "error-404.dd29d79a.css",
+    "resourceType": "style"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.css": {
+    "resourceType": "style",
+    "file": "error-500.26873dcc.css",
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.css"
+  },
+  "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue": {
+    "resourceType": "script",
+    "module": true,
+    "css": [],
+    "file": "error-500.1fd4b925.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.0_@types+node@18.13.0/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+  },
+  "error-500.26873dcc.css": {
+    "file": "error-500.26873dcc.css",
+    "resourceType": "style"
+  },
+  "node_modules/.pnpm/nuxt@3.2.0_@types+node@18.13.0/node_modules/nuxt/dist/app/entry.css": {
+    "resourceType": "style",
+    "file": "entry.dd64dcb9.css",
+    "src": "node_modules/.pnpm/nuxt@3.2.0_@types+node@18.13.0/node_modules/nuxt/dist/app/entry.css"
+  },
+  "node_modules/.pnpm/nuxt@3.2.0_@types+node@18.13.0/node_modules/nuxt/dist/app/entry.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "css": [
+      "entry.dd64dcb9.css"
+    ],
+    "dynamicImports": [
+      "virtual:nuxt:/Volumes/CODE-LABX/Packages/nuxt-alt/nuxt-3-auth/.nuxt/error-component.mjs"
+    ],
+    "file": "entry.cde091b3.js",
+    "isEntry": true,
+    "src": "node_modules/.pnpm/nuxt@3.2.0_@types+node@18.13.0/node_modules/nuxt/dist/app/entry.mjs"
+  },
+  "entry.dd64dcb9.css": {
+    "file": "entry.dd64dcb9.css",
+    "resourceType": "style"
+  },
+  "virtual:nuxt:/Volumes/CODE-LABX/Packages/nuxt-alt/nuxt-3-auth/.nuxt/error-component.mjs": {
+    "resourceType": "script",
+    "module": true,
+    "dynamicImports": [
+      "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-404.vue",
+      "node_modules/.pnpm/@nuxt+ui-templates@1.1.1/node_modules/@nuxt/ui-templates/dist/templates/error-500.vue"
+    ],
+    "file": "error-component.c5cc4c19.js",
+    "imports": [
+      "node_modules/.pnpm/nuxt@3.2.0_@types+node@18.13.0/node_modules/nuxt/dist/app/entry.mjs"
+    ],
+    "isDynamicEntry": true,
+    "src": "virtual:nuxt:/Volumes/CODE-LABX/Packages/nuxt-alt/nuxt-3-auth/.nuxt/error-component.mjs"
+  }
+};
+
+export { client_manifest as default };
+//# sourceMappingURL=client.manifest.mjs.map
---
+++ .output/server/index.mjs
@@ -0,0 +1,18 @@
+globalThis._importMeta_={url:import.meta.url,env:process.env};import 'node-fetch-native/polyfill';
+import 'node:http';
+import 'node:https';
+import 'destr';
+import './chunks/nitro/config.mjs';
+export { n as default } from './chunks/nitro/node-server.mjs';
+import 'ufo';
+import 'scule';
+import 'ofetch';
+import 'unenv/runtime/fetch/index';
+import 'hookable';
+import 'ohash';
+import 'unstorage';
+import 'defu';
+import 'node:fs';
+import 'node:url';
+import 'pathe';
+//# sourceMappingURL=index.mjs.map
